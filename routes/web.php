51df50c899b3413a4e980b207ef7ffc6e648350c
Your Name <you@example.com>
2026-07-27 20:10:07 +0700

    update huruf auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\AdminAuthController;
 
 Route::get('/', function () {
