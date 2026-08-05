bc93731520d77d05db67895c7f5a15ab7760572c
Your Name <you@example.com>
2026-08-05 19:30:15 +0700

    import FontAwesome stylesheet to enable landing page chat icons

--- resources/views/informasi/index.blade.php
+++ resources/views/informasi/index.blade.php
@@ -12,6 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800;900&display=swap"
         rel="stylesheet">
 
+    <!-- FontAwesome Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.1/css/all.min.css">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Swiper.js CSS and JS -->
